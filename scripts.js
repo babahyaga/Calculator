8d86b1f4ea11076c83909e317580b0a11f7e6110
Stephen Lang <stephenlang91@gmail.com>
2018-11-01 23:16:46 -0400

    added functionality to event listeners to alloy for calculation.  Created an operate function.  Now project can perform simple calculations.  Bug with long numbers/decimals breaking through the display persists.

--- scripts.js
+++ scripts.js
@@ -2,44 +2,141 @@
 
 var display = document.getElementById('display');
 
+var operator = ' ';
+var num1 = 5;
+var num2 = 6;
+var result = ' ';
+
+
+
 //functions
-let add = (a, b) => a + b;
-let subtract = (a, b) => a - b;
-let multiply = (a, b) => a * b;
-let divide = (a, b) => a / b;
+let add = (num1, num2) => Number(num1) + Number(num2);
+let subtract = (num1, num2) => Number(num1) - Number(num2);
+let multiply = (num1, num2) => Number(num1) * Number(num2);
+let divide = (num1, num2) => Number(num1) / Number(num2);
 
 
-
-var operator = '/';
-var a = 5;
-var b = 6;
-
+let operate = () => {
 switch(operator){
-    case '+': add(a,b);
+    case '+': result = add(num1,num2);
     break;
-    case '-': subtract(a,b);
+    case '-': result = subtract(num1,num2);
     break;
-    case '*': multiply(a,b);
+    case '*': result = multiply(num1,num2);
     break;
-    case '/': divide(a,b);
+    case '/': result = divide(num1,num2);
+}
+operator = ' ';
+display.textContent = result;
 };
 
-//add event listeners to all the buttons
+//add event listeners to all the buttons, update the display, update nums, update operator, make equals work
 
 document.getElementById("one").addEventListener("click", function() { 
-    display.textContent = '1';
-});
-document.getElementById("two").addEventListener("click", function(e) { display.textContent = '2'});
-document.getElementById("three").addEventListener("click", function(e) { display.textContent = '3'});
-document.getElementById("four").addEventListener("click", function(e) { display.textContent = '4'});
-document.getElementById("five").addEventListener("click", function(e) { display.textContent = '5'});
-document.getElementById("six").addEventListener("click", function(e) { display.textContent = '6'});
-document.getElementById("seven").addEventListener("click", function(e) { display.textContent = '7'});
-document.getElementById("eight").addEventListener("click", function(e) { display.textContent = '8'});
-document.getElementById("nine").addEventListener("click", function(e) { display.textContent = '9'});
-document.getElementById("zero").addEventListener("click", function(e) { display.textContent = '0'});
-document.getElementById("plus").addEventListener("click", function(e) { display.textContent = '+'});
-document.getElementById("minus").addEventListener("click", function(e) { display.textContent = '-'});
-document.getElementById("division").addEventListener("click", function(e) { display.textContent = '/'});
-document.getElementById("multiply").addEventListener("click", function(e) { display.textContent = '*'});
-document.getElementById("clear").addEventListener("click", function(e) { display.textContent = ' '});
+    if (operator === ' '){
+        num1 += '1';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '1'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("two").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '2';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '2'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("three").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '3';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '3'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("four").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '4';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '4'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("five").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '5';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '5'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("six").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '6';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '6'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("seven").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '7';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '7'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("eight").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '8';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '8'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("nine").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '9';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '9'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("zero").addEventListener("click", function(e) { 
+    if (operator === ' '){
+        num1 += '0';
+        display.textContent = num1;
+        } else if (operator !== ' '){
+        num2 += '0'
+        display.textContent = num2;
+        }
+    });
+document.getElementById("plus").addEventListener("click", function(e) { 
+    operator = '+';
+    display.textContent = '+'});
+document.getElementById("minus").addEventListener("click", function(e) { 
+    operator = '-';
+    display.textContent = '-'});
+document.getElementById("division").addEventListener("click", function(e) { 
+    operator = '/';
+    display.textContent = '/'});
+document.getElementById("multiply").addEventListener("click", function(e) { 
+    operator = '*';
+    display.textContent = '*'});
+document.getElementById("clear").addEventListener("click", function(e) { 
+    num1 = ' ';
+    num2 = ' ';
+    operator = ' ';
+    display.textContent = ' '});
+document.getElementById("equals").addEventListener("click", operate);
